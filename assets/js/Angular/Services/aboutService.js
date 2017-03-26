5a7279e018f08222e3dbd8c04b60d13981564402
Leonardo Rocha <leonardorochacastellano@gmail.com>
2017-03-26 18:41:34 -0300

    Criado serviços e colocado as listas em seu devido serviço

---
+++ assets/js/Angular/Services/aboutService.js
@@ -0,0 +1,32 @@
+angular.module("portfolio").service('aboutService', function () {
+    this.sections = [
+        {id: 1, nome: "linguagens", section: "Linguagens"},
+        {id: 2, nome: "bancoDados", section: "Banco de Dados"},
+        {id: 3, nome: "habilidades", section: "Habilidades"}
+    ];
+    this.itensSection = [
+        {nome: "PHP", porcentagem: 80, section: 1},
+        {nome: "C#", porcentagem: 70, section: 1},
+        {nome: "JAVA", porcentagem: 40, section: 1},
+        {nome: "HTML", porcentagem: 70, section: 1},
+        {nome: "CSS", porcentagem: 60, section: 1},
+        {nome: "SQL", porcentagem: 80, section: 2},
+        {nome: "MySQL", porcentagem: 95, section: 2},
+        {nome: "Oracle", porcentagem: 60, section: 2},
+        {nome: "Pusher", porcentagem: 40, section: 2},
+        {nome: "SQLite", porcentagem: 50, section: 2},
+        {nome: "Capacidade de ensinar", porcentagem: 80, section: 3},
+        {nome: "Capacidade de aprender rapidamente", porcentagem: 80, section: 3},
+        {nome: "Pró-atividade", porcentagem: 100, section: 3},
+        {nome: "Paixão pela área de TI", porcentagem: 100, section: 3},
+        {nome: "Capacidade de trabalhar sob pressão", porcentagem: 80, section: 3},
+        {nome: "Capacidade de inovar", porcentagem: 70, section: 3}
+    ];
+
+    this.listaSections = function () {
+        return this.sections;
+    };
+    this.listaItensSection = function(){
+        return this.itensSection;
+    };
+});
